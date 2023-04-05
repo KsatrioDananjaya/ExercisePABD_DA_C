6bfffca68e78e9792bfc0ba8b01197d28d359820
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2023-04-05 16:02:27 +0700

    Add project files.

---
+++ exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Insert_And_Get_Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+            while(true)
+            {
+                try
+                {
+                    Console.WriteLine("Koneksi ke Database\n");
+                    Console.WriteLine("Masukkan ke user ID : ");
+                    string user = Console.ReadLine();
+
+                }
+            }
+        }
+    }
+}
